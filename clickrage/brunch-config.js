7ec3f4804149d0849d055e3b152d727de5418d26
billbunkum <william.richard.hurst@gmail.com>
2017-05-08 15:11:45 -0400

    fix file structure for CSS

--- clickrage/brunch-config.js
+++ clickrage/brunch-config.js
@@ -1,5 +1,11 @@
 // See http://brunch.io for documentation.
 exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
+    javascripts: {
+        joinTo: 'app.js'
+    },
+    stylesheets: {
+        joinTo: {
+            'app.css': 'app/stylesheets/*.css'
+        }
+    }
 };
